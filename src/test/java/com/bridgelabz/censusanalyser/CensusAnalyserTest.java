22f7358159a5e641484d506889eaee5d8627f8f7
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-26 00:52:13 +0530

    [abhishek] add: TC 1.2 if incorrect should throw custom exception

--- src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/censusanalyser/CensusAnalyserTest.java
@@ -1,20 +1,27 @@
-package bridgelabz.censusanalyser;
+package com.bridgelabz.censusanalyser;
 
 import org.junit.Test;
 import org.junit.Assert;
-import com.bridgelabz.censusanalyser.CensusAnalyserException;
-import com.bridgelabz.censusanalyser.CensusAnlayser;
 
 public class CensusAnalyserTest {
 	private static final String INDIA_CENSUS_FILE_PATH = "C:\\Users\\abhis\\eclipse-workspace\\day21assignment\\src\\main\\java\\com\\bridgelabz\\censusanalyser\\IndiaStateCensusData.csv";
-
+	private static final String WRONG_FILE_PATH ="";
 	@Test
-	public void givenIndianCensusCorrectRecords() {
+	public void givenIndianCensusData_ShouldReturnNoOfRecords() {
 		try {
 			CensusAnlayser censusAnalyser = new CensusAnlayser();
 			int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_FILE_PATH);
 			Assert.assertEquals(5, numOfRecords);
 		} catch (CensusAnalyserException e) {
+		}
+	}
+	@Test
+	public void givenIndianCensusData_WithWrongFile_ShouldThrowExceptionCorrect() {
+		try {
+			CensusAnlayser censusAnalyser = new CensusAnlayser();
+		    censusAnalyser.loadIndiaCensusData(WRONG_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
 		}
 	}
 }
